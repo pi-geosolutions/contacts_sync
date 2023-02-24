154502835f5dc38bc4c51d71b6e7a4da7d9e8722
Jean Pommier <jean.pommier@pi-geosolutions.fr>
2023-02-24 09:45:56 +0100

    Make the path a config option (api2xml script)

--- src/contacts_api_to_xml.py
+++ src/contacts_api_to_xml.py
@@ -1,5 +1,6 @@
 import click
 from jinja2 import Template
+from pathlib import Path
 import requests
 
 session = requests.Session()
@@ -21,9 +22,13 @@ def get_pocs(api_url):
 @click.command()
 @click.option('--api_url', default=default_api_url, help='URL to the contact point reference service')
 @click.option('--template_file', default='templates/poc_xml.j2', help='Jinja2 template to structure each POC')
+@click.option('--destination_folder', default='build/', help='Folder where the XML file will be written')
 def api_harvest(api_url,
-                template_file
+                template_file,
+                destination_folder,
                 ):
+    # Make sure the destination folder exists
+    Path(destination_folder).mkdir(parents=True, exist_ok=True)
     for page in get_pocs(api_url):
         if not page.get('_embedded', None):
             break
@@ -31,7 +36,7 @@ def api_harvest(api_url,
             with open(template_file) as file_:
                 template = Template(file_.read())
                 poc_xml = template.render(poc=entry)
-                with open(f'build/{entry["organizationCode"]}.xml', 'w') as f:
+                with open(f'{destination_folder}/{entry["organizationCode"]}.xml', 'w') as f:
                     f.write(poc_xml)
             print(entry['enrichedLabel'])
 
